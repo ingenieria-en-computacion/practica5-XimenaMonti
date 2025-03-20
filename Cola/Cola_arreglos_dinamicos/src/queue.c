de68b58736d642719c1727033fa5cdd68fa10a51
XimenaMontielGuerrero <xmontielguerrero@gmail.com>
2025-03-20 17:20:38 -0600

    Archivos Cola_arreglos_dinamicos

--- Cola/Cola_arreglos_dinamicos/src/queue.c
+++ Cola/Cola_arreglos_dinamicos/src/queue.c
@@ -9,7 +9,19 @@
  * @details Esta función inicializa una cola vacía. Asigna memoria dinàmica con malloc al arreglo data usando len
  */
 Queue queue_create(int len){
+    Queue q;
+    q.data = (Data*)malloc(len * sizeof(Data)); // se asigna memoria dinámica al arreglo data
+    if (q.data == NULL) {  
+        q.head = -1;
+        q.tail = -1;
+        q.len = 0;  
+    } else {
+        q.head = -1;
+        q.tail = -1;
+        q.len = len;
+    }
 
+    return q;
 }
 
 /**
@@ -20,7 +32,11 @@ Queue queue_create(int len){
  * @details Esta función añade el dato `d` al final de la cola.
  */
 void queue_enqueue(Queue* q, Data d){
-
+    if(q->head == -1){
+        q->head = 0;
+    }
+    q->tail++;
+    q->data[q->tail] = d;// se inserta el dato en la cola
 }
 
 /**
@@ -33,7 +49,16 @@ void queue_enqueue(Queue* q, Data d){
  *          Si la cola está vacía, no se realiza ninguna operación y se devuelve un valor de error.
  */
 Data queue_dequeue(Queue* q){
-
+    if(q->head == -1){ // -1 indica que la cola está vacía, y q->head es el frente de la cola porque es el primer dato que se inserto
+        return -1;
+    }
+    Data d = q->data[q->head]; // data es un arreglo de datos, y q-> head es el frente de la cola  y  
+    q->head++;
+    if(q->head > q->tail){
+        q->head = -1;
+        q->tail = -1;
+    }
+    return d;
 }
 
 /**
@@ -45,7 +70,10 @@ Data queue_dequeue(Queue* q){
  *          como `queue_dequeue` en una cola vacía.
  */
 bool queue_is_empty(Queue* q){
-
+    if(q->head == -1){// se pone tail ya que es el final de la cola 
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -57,17 +85,21 @@ bool queue_is_empty(Queue* q){
  *          Si la cola está vacía, no se realiza ninguna operación y se devuelve un valor de error.
  */
 Data queue_front(Queue* q){
-
+    if(q->head == -1){
+        return -1;
+    }
+    return q->data[q->head];
 }
 
 /**
  * Vacía la cola, eliminando todos sus elementos.
  * 
- * @param q Referemcoa a la cola que se desea vaciar.
+ * @param q Referemcia a la cola que se desea vaciar.
  * @details Esta función hace que los índices head y tail tomen el valor de -1
  */
 void queue_empty(Queue* q){
-
+    q->head = -1;
+    q->tail = -1;
 }
 
 /**
@@ -79,5 +111,8 @@ void queue_empty(Queue* q){
  *          de ser eliminada.
  */
 void queue_delete(Queue* q){
-
+    free(q->data);
+    q->head = -1;
+    q->tail = -1;
+    q->len = 0;
 }
--- Cola/Cola_arreglos_dinamicos/src/queue.h
+++ Cola/Cola_arreglos_dinamicos/src/queue.h
@@ -6,17 +6,17 @@ typedef int Data;
 
 typedef struct {
     Data *data;
-    int head;
-    int tail;
-    int len
+    int head;// head es el frente de la cola
+    int tail;// tail es el final de la cola
+    int len;// len es la cantidad de datos que se pueden guardar en la cola
 } Queue;
 
-Queue queue_create(int len);
-void queue_enqueue(Queue* , Data);
-Data queue_dequeue(Queue*);
+Queue queue_create(int len);//crea una nueva cola vacía y la devuelve
+void queue_enqueue(Queue* , Data);// se inserta un dato en la cola
+Data queue_dequeue(Queue*); 
 bool queue_is_empty(Queue*);
-Data queue_front(Queue*);
-void queue_empty(Queue*);
+Data queue_front(Queue*);// se obtiene el dato al frente de la cola
+void queue_empty(Queue*);// vacía la cola
 void queue_delete(Queue*);
 
 #endif // __QUEUE_H__
